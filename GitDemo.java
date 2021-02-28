510a52f4db8559dc24a4c892e690df0d4f8710fd
vbharga5 <79557520+vbharga5@users.noreply.github.com>
2021-02-27 17:45:23 -0800

    Update GitDemo.java
    added name to line 2 
    computed sum on line 18
    returned sum on line 19
    declared new private variable reverseNum on line 7
    set newNumber to reverseNum on line 50
    returned reverseNum on line 51

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+ Vidhi Bhargava
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum;
     
     public GitDemo() {
         previousNumber = 1;
@@ -14,7 +15,8 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
-        return 0;
+        double sum = previousNumber + num;
+        return sum;
     }
     
     /*****
@@ -45,8 +47,8 @@ public class GitDemo {
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
-        
-        return newNumber;
+        reverseNum = newNumber;
+        return reverseNum;
     }
     
 }
